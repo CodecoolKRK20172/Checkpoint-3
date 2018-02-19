7e2458c0625e1b85bd255a032b52f3d6536c0d22
Justyna Fornalik <jfornalik@gmail.com>
2018-02-19 10:05:44 +0100

    Add Book model class

---
+++ src/model/Book.java
@@ -0,0 +1,48 @@
+package model;
+
+public class Book {
+    private int ISBN;
+    private String author;
+    private String title;
+    private String publisher;
+    private int publicationYear;
+    private int price;
+
+    Book(int ISBN, String author, String title, String publisher, int publicationYear, int price) {
+    this.ISBN = ISBN;
+    this.author = author;
+    this.title = title;
+    this.publisher = publisher;
+    this.publicationYear = publicationYear;
+    this.price = price;
+    }
+
+    public String getFullBookInfo(){
+        return String.format("%s: %s", author, title);
+    }
+
+
+    public int getISBN() {
+        return ISBN;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
