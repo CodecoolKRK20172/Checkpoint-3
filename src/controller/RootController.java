f8bbf904e006185808caf2dc28c5c16a9ba1ceee
Justyna Fornalik <jfornalik@gmail.com>
2018-02-19 11:24:25 +0100

    Create RootController class

---
+++ src/controller/RootController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import view.BaseView;
+
+public class RootController {
+
+    private BaseView view =new BaseView();
+    private BookController bookController = new BookController();
+
+    public void start () {
+
+        String option = "";
+
+        while (!option.equals("0")) {
+
+            view.printMenu();
+            option = view.getInput("Enter option: ");
+
+            switch (option) {
+                case "1":
+                    bookController.showBooks();
+                    break;
+            }
+        }
+    }
+}
